05fb294f923dff98dacc21cc6374b2a3cc12dde1
perdanaph <danangputro9@gmail.com>
2024-01-01 03:55:14 +0700

    wip

--- frameworks/webserver/express.js
+++ frameworks/webserver/express.js
@@ -7,7 +7,7 @@ function expressConfig (app, express) {
   app.use('/product', express.static(path.join(__dirname, 'public', 'product-images')));
   app.use(cors())
   app.use(bodyParser.urlencoded({ extended: true }));
-  app.use(bodyParser.json())
+  app.use(bodyParser.json());
 }
 
 module.exports = expressConfig
