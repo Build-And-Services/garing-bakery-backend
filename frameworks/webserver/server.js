436335b7e4a9ddce8dbebef12b14522a2d12ae50
Heri Setyawan <herisetyawan233@gmail.com>
2024-01-02 04:20:31 +0700

    feat/custom-terminal: add little bit color and style

--- frameworks/webserver/server.js
+++ frameworks/webserver/server.js
@@ -5,8 +5,10 @@ function serverConfig(app) {
     const host = process.env.BASE_URL || 'http://localhost';
     const port = process.env.PORT || 3000;
     try {
-      app.listen(port, () => console.log(`Server is listening on ${host}:${port}`));
-      LogRouter(app);
+      app.listen(port, () => {
+        console.log(`Server is listening on ${host}:${port}`);
+        LogRouter(app);
+      });
     } catch (error) {
       console.log('Error: ', error);
     }
