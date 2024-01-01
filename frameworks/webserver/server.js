5713bcaa7beb252977980e730d421e1d379ded1a
Heri Setyawan <herisetyawan233@gmail.com>
2024-01-02 04:05:09 +0700

    feat/custom-terminal: display all route

--- frameworks/webserver/server.js
+++ frameworks/webserver/server.js
@@ -1,11 +1,14 @@
+const LogRouter = require('../../app/utils/custom-log');
+
 function serverConfig(app) {
   function start() {
     const host = process.env.BASE_URL || 'http://localhost';
     const port = process.env.PORT || 3000;
     try {
       app.listen(port, () => console.log(`Server is listening on ${host}:${port}`));
+      LogRouter(app);
     } catch (error) {
-      console.log('ini test', error);
+      console.log('Error: ', error);
     }
   }
   start();
