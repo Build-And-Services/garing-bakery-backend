f12b62c8a71e919e9603ef5a3b5ebbfe893a0424
Heri Setyawan <herisetyawan233@gmail.com>
2024-01-02 04:20:31 +0700

    feat/custom-terminal: add little bit color and style

--- app/utils/custom-log/index.js
+++ app/utils/custom-log/index.js
@@ -9,7 +9,31 @@ function LogRouter(app) {
     pathApi.handle.stack.forEach((parameter) => {
       const suffix = parameter.route.path;
       parameter.route.stack.forEach((last) => {
-        console.warn(`[${last.method}]  ${process.env.BASE_URL}${prefix + prefixApi + suffix}`);
+        const red = '\x1b[31m';
+        const green = '\x1b[32m';
+        const yellow = '\x1b[33m';
+        const blue = '\x1b[34m';
+        const magenta = '\x1b[35m';
+        const cyan = '\x1b[36m';
+        const white = '\x1b[37m';
+        let color = '';
+        if (last.method == 'delete') {
+          color = red;
+        } else if (last.method == 'put') {
+          color = yellow;
+        } else if (last.method == 'post') {
+          color = magenta;
+        } else if (last.method == 'get') {
+          color = cyan;
+        } else {
+          color = blue;
+        }
+
+        console.warn(
+          `${color}[${last.method.toUpperCase()}] ${green}=>  ${white + process.env.BASE_URL}:${
+            process.env.PORT
+          }${prefix + prefixApi + suffix}`
+        );
       });
     });
   });
