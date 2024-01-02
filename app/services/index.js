5f7daaaf6102069f5026c0286caa3ddf294f2293
rafikusdiarto <rafikusdiarto1707@gmail.com>
2024-01-02 16:30:59 +0700

    feat:manage categories

--- app/services/index.js
+++ app/services/index.js
@@ -1,7 +1,9 @@
 const AuthenticationService = require('./auth-service');
 const ProductService = require('./product-service');
+const CategoryService = require('./category-service');
 
 module.exports = {
   AuthenticationService,
   ProductService,
+  CategoryService,
 };
